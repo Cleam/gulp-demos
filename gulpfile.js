cb96692e22b6742c1ef4eddeb976b455cf89c003
lizhigaomac <lizhigao@021.com>
2017-09-17 23:58:34 +0800

    gulp demo2

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,38 @@
 var gulp = require('gulp')
 var browserSync = require('browser-sync').create()
+var sass = require('gulp-sass')
 
-// Static server
-gulp.task('browser-sync', function () {
+/* =================================
+  demo1
+ ================================= */
+// gulp.task('browser-sync', function () {
+//   browserSync.init({
+//     server: {
+//       baseDir: './src/'
+//     }
+//   })
+// })
+
+// gulp.task('default', ['browser-sync'])
+
+/* =================================
+  demo2
+ ================================= */
+gulp.task('serve', ['sass'], function() {
   browserSync.init({
     server: {
       baseDir: './src/'
     }
   })
+  gulp.watch('./src/scss/*.scss', ['sass'])
+  gulp.watch('./src/*.html').on('change', browserSync.reload)
 })
 
-gulp.task('default', ['browser-sync'])
+gulp.task('sass', function(){
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('default', ['serve'])
