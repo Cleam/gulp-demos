d3ed68dc40c6db8e88b432a2ba308a074becb06e
lizhigaomac <lizhigao@021.com>
2017-09-18 11:56:11 +0800

    gulp-demo3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 var browserSync = require('browser-sync').create()
-var sass = require('gulp-sass')
+var sass = require('gulp-ruby-sass')
+var sourcemaps = require('gulp-sourcemaps')
 
 /* =================================
   demo1
@@ -18,21 +19,45 @@ var sass = require('gulp-sass')
 /* =================================
   demo2
  ================================= */
-gulp.task('serve', ['sass'], function() {
+// gulp.task('serve', ['sass'], function() {
+//   browserSync.init({
+//     server: {
+//       baseDir: './src/'
+//     }
+//   })
+//   gulp.watch('./src/scss/*.scss', ['sass'])
+//   gulp.watch('./src/*.html').on('change', browserSync.reload)
+// })
+
+// gulp.task('sass', function(){
+//   return gulp.src('./src/scss/*.scss')
+//     .pipe(sass())
+//     .pipe(gulp.dest('./src/css'))
+//     .pipe(browserSync.stream())
+// })
+
+// gulp.task('default', ['serve'])
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function () {
   browserSync.init({
-    server: {
-      baseDir: './src/'
-    }
+    server: './src'
   })
-  gulp.watch('./src/scss/*.scss', ['sass'])
-  gulp.watch('./src/*.html').on('change', browserSync.reload)
+  gulp.watch('src/scss/*.scss', ['sass'])
+  gulp.watch('src/*.html').on('change', browserSync.reload)
 })
 
-gulp.task('sass', function(){
-  return gulp.src('./src/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./src/css'))
-    .pipe(browserSync.stream())
+gulp.task('sass', () => {
+  return sass('src/scss/*.scss', {sourcemap: true})
+    .on('error', function (err) {
+      console.error('Error!', err.message)
+    })
+    .pipe(sourcemaps.write('./', {
+      includeContent: false,
+      sourceRoot: 'src/scss'
+    }))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream({match: '**/*.css'}))
 })
 
 gulp.task('default', ['serve'])
